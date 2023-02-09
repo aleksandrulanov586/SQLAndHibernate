f4958e8558586dcd99d993cf612e24d5a22b44fe
aleksandrulanov586 <123461934+aleksandrulanov586@users.noreply.github.com>
2023-02-09 15:01:33 +0300

    подключения jdbc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,30 @@
- package org.example;
+package org.example;
 
- import java.sql.Connection;
- import java.sql.DriverManager;
- import java.sql.ResultSet;
- import java.sql.SQLException;
- import java.sql.Statement;
- import org.hibernate.Session;
- import org.hibernate.SessionFactory;
- import org.hibernate.boot.Metadata;
- import org.hibernate.boot.MetadataSources;
- import org.hibernate.boot.registry.StandardServiceRegistry;
- import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
- public class Main {
+public class Main {
 
-   public static void main(String[] args) throws SQLException {
+  public static void main(String[] args) throws SQLException {
+    String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC";
+    String user = "root";
+    String pass = "ytafhbfy12";
 
-     StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure(
-         "hibernate.cfg.xml").build();
-     Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
-     SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-     Session session = sessionFactory.openSession();
-     Course course = session.get(Course.class, 1);
-     System.out.println(course.getName());
-     sessionFactory.close();
+    Connection connection = DriverManager.getConnection(url, user, pass);
+    Statement statement = connection.createStatement();
+    statement.execute("SELECT pl.course_name, pl.subscription_date FROM PurchaseList pl WHERE pl.course_name = \"Веб-разработчик c 0 до PRO\"ORDER BY pl.subscription_date;");
+    ResultSet resultSet = statement.executeQuery("SELECT * FROM Students s WHERE MONTH(s.registration_date ) = 4");
+    while (resultSet.next()) {
+      String courseName = resultSet.getString("name");
+      System.out.println(courseName);
+    }
+    resultSet.close();
+    statement.close();
+    connection.close();
 
-   }
- }
+
+  }
+}
